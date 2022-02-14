340a717b251a6039e1ff3cdbe08da14df1a8ad31
Mahamat <annour.mahamat-ext@kering.com>
2022-02-14 12:06:03 +0100

    add routing to articles

--- src/app/views/articles/home/home.component.ts
+++ src/app/views/articles/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { ArticlesService } from "../articles.service";
 
@@ -11,10 +12,17 @@ export class HomeComponent implements OnInit {
   user = "Mahamat";
   archivedArticle$;
   articles$;
- 
-  constructor(private readonly articleService: ArticlesService) {}
+
+  constructor(
+    private readonly articleService: ArticlesService,
+    private readonly router: Router
+  ) {}
   ngOnInit(): void {
     this.archivedArticle$ = this.articleService.getArchivedArticle();
     this.articles$ = this.articleService.getArticles();
+  }
+
+  showArticle(article) {
+    this.router.navigate(["/articles", article.id]);
   }
 }
