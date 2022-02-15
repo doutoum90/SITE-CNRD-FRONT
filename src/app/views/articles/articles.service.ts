6f487135863b3d201ba73591f0b59a19664cd1e2
Mahamat <annour.mahamat-ext@kering.com>
2022-02-15 19:48:34 +0100

    manage read partially the article

--- src/app/views/articles/articles.service.ts
+++ src/app/views/articles/articles.service.ts
@@ -148,6 +148,66 @@ export class ArticlesService {
       datePublication: new Date("24 December 2019"),
       icon: "library_books",
     },
+
+    {
+      id: "EREGHERHREGHREHERG",
+      title: "Data structure test",
+      isArchived: false,
+      content:
+        "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p><br></p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p><br></p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p><br></p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p><br></p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p><br></p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p><br></p>",
+      shortContent:
+        "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p>",
+      datePublication: new Date("23 December 2019"),
+      icon: "view_week",
+      commentaires: [
+        {
+          id: "gheghergreh",
+          content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Lorem ipsum dolor sit amet, <a href="#">#consecteturadipiscing </a>.`,
+          datePublication: new Date("23 December 2019"),
+          auteur: "John Doe",
+          user: {
+            id: "gheghergreh",
+            image: "https://bootdey.com/img/Content/user_1.jpg",
+            userName: "@MartinoMon",
+          },
+        },
+        {
+          id: "EZZEKZEHZEJEZHEZJ",
+          content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Lorem ipsum dolor sit amet, <a href="#">#consecteturadipiscing </a>.`,
+          datePublication: new Date("23 December 2019"),
+          auteur: "John Doe",
+          user: {
+            id: "gheghergreh",
+            image: "https://bootdey.com/img/Content/user_2.jpg",
+            userName: "@LaurenceCorreil",
+          },
+        },
+        {
+          id: "gheghergreh",
+          content: ` Lorem ipsum dolor <a href="#">#sitamet</a> sit amet, consectetur adipiscing elit.`,
+          datePublication: new Date("23 December 2019"),
+          auteur: "John Doe",
+          user: {
+            id: "gheghergreh",
+            image: "https://bootdey.com/img/Content/user_3.jpg",
+            userName: "@JohnNida",
+          },
+        },
+      ],
+    },
+    {
+      id: "EZEZGHZEJZEHJZEJEZK",
+      title: "Design pattern test",
+      isArchived: false,
+      content:
+        "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p><br></p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p><br></p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p><br></p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p><br></p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p><br></p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p><br></p>",
+      shortContent:
+        "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit, recusandae! Modi fuga fugit quo aut optio odit harum cum, enim ipsa illo reprehenderit nulla itaque omnis odio magni deserunt ipsum!</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis nisi consequuntur totam provident molestias laboriosam quia illum? Dolorum illo, nobis suscipit placeat vero voluptatem tenetur totam pariatur quaerat deleniti molestiae?</p>",
+      datePublication: new Date("24 December 2019"),
+      icon: "library_books",
+    },
   ];
   constructor() {}
 
--- src/app/views/articles/home/home.component.ts
+++ src/app/views/articles/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { TranslateService } from "@ngx-translate/core";
 import { Observable } from "rxjs";
 import { ArticlesService } from "../articles.service";
 import { Article } from "../model/article.model";
@@ -13,10 +14,13 @@ export class HomeComponent implements OnInit {
   user = "Mahamat";
   archivedArticle$: Observable<Article[]>;
   articles$: Observable<Article[]>;
+  readMore = undefined;
+  readMoreLabel = "";
 
   constructor(
     private readonly articleService: ArticlesService,
-    private readonly router: Router
+    private readonly router: Router,
+    private readonly translate: TranslateService
   ) {}
   ngOnInit(): void {
     this.archivedArticle$ = this.articleService.getArchivedArticle();
@@ -25,5 +29,16 @@ export class HomeComponent implements OnInit {
 
   showArticle(article) {
     this.router.navigate(["/articles", article.id]);
+  }
+
+  concatReadMore(content: string, index: number) {
+    const suite = ``;
+
+    return content + suite;
+  }
+
+  readMoreArticle(index: number) {
+    console.log(index);
+    this.readMore = index;
   }
 }
